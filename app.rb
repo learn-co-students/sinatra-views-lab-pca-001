d03b33f4f0a04db243b35da405f7e92d40535fea
Pat McClernan <patmccler@gmail.com>
2020-06-04 17:19:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
+require "date"
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
+  get "/" do
+    erb :index
+  end
 
+  get "/hello" do
+    erb :hello
+  end
 
+  get "/goodbye" do
+    "Goodbye Joe"
+  end
+
+  get "/date" do
+    erb :date
+  end
 end
